97d2204a706d8b78ba01fb6b38a828bb7c0fa58e
Sue Mu <mushuyun@yahoo.com>
2020-04-28 11:01:50 -0400

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 var compression = require('compression');
-var compression = require('compression');
 
 const PORT = process.env.PORT || 3000;
 
