550b0a6c2ed32e8badee70a83611c0ab4511173f
mushuyun <mushuyun@yahoo.com>
2020-04-27 22:47:43 -0400

    heroku deploy

--- server.js
+++ server.js
@@ -1,19 +1,32 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+//var compression = require('compression');
+var compression = require('compression');
 
 const PORT = process.env.PORT || 3000;
 
 const workout = require("./models");
 
 const app = express();
+app.use(compression({ filter: shouldCompress }));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://sue1:Tha,bto7@ds255794.mlab.com:55794/heroku_gxtw33q8", { useNewUrlParser: true });
+
+function shouldCompress (req, res) {
+  if (req.headers['x-no-compression']) {
+    // don't compress responses with this request header
+    return false
+  }
+ 
+  // fallback to standard filter function
+  return compression.filter(req, res)
+}
 
 //code stars here
 require("./routes/apiRoutes.js")(app);
